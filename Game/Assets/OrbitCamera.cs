b3f8d1ef85fc6fe2a0dc52eff8a91c8f531f3c78
Nuno Silva <little.coding.fox@gmail.com>
2024-03-07 08:05:16 +0000

    More work on orbit camera;

--- Game/Assets/OrbitCamera.cs
+++ Game/Assets/OrbitCamera.cs
@@ -8,8 +8,12 @@ class OrbitCamera : IComponent
     public float distance = 5.0f;
     public float focusRadius = 1.0f;
     public float focusCentering = 0.5f;
+    public float rotationSpeed = 90;
+    public float minVerticalAngle = -90;
+    public float maxVerticalAngle = 60;
 
     internal Transform focus;
 
     internal Vector3 focusPosition;
+    internal Vector2 orbitAngles = new Vector2(45, 0);
 }
--- Game/Assets/OrbitCameraSystem.cs
+++ Game/Assets/OrbitCameraSystem.cs
@@ -7,6 +7,64 @@ class OrbitCameraSystem : IEntitySystem
 {
     public SubsystemType UpdateType => SubsystemType.Update;
 
+    private void UpdateFocusPoint(Transform transform, OrbitCamera camera)
+    {
+        var target = camera.focus.Position;
+
+        if (camera.focusRadius > 0)
+        {
+            var distance = Vector3.Distance(target, camera.focusPosition);
+
+            var t = 1.0f;
+
+            if (distance > 0.01f && camera.focusCentering > 0)
+            {
+                t = Math.Pow(1.0f - camera.focusCentering, Time.deltaTime);
+            }
+
+            if (distance > camera.focusRadius)
+            {
+                t = Math.Min(t, camera.focusRadius / distance);
+            }
+
+            camera.focusPosition = Vector3.Lerp(target, camera.focusPosition, t);
+        }
+        else
+        {
+            camera.focusPosition = target;
+        }
+    }
+
+    private bool UpdateRotation(OrbitCamera camera)
+    {
+        var input = new Vector2(-Input.MouseRelativePosition.Y, -Input.MouseRelativePosition.X);
+
+        var e = 0.001f;
+
+        if(input.X < -e || input.X > e || input.Y < -e || input.Y > e)
+        {
+            camera.orbitAngles += input * camera.rotationSpeed * Time.deltaTime;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private void ConstrainAngles(OrbitCamera camera)
+    {
+        camera.orbitAngles.X = Math.Clamp(camera.orbitAngles.X, camera.minVerticalAngle, camera.maxVerticalAngle);
+
+        if(camera.orbitAngles.Y < 0)
+        {
+            camera.orbitAngles.Y += 360;
+        }
+        else if(camera.orbitAngles.Y >= 360)
+        {
+            camera.orbitAngles.Y -= 360;
+        }
+    }
+
     public void Process(float deltaTime)
     {
         Scene.ForEach((Entity entity, bool enabled, ref Transform transform, ref OrbitCamera camera) =>
@@ -26,32 +84,30 @@ class OrbitCameraSystem : IEntitySystem
                 }
 
                 camera.focusPosition = camera.focus.Position;
+                transform.LocalRotation = Math.FromEulerAngles(new Vector3(camera.orbitAngles.X, camera.orbitAngles.Y, 0));
             }
 
-            var target = camera.focus.Position;
+            UpdateFocusPoint(transform, camera);
 
-            if(camera.focusRadius > 0)
+            Quaternion rotation;
+
+            if(UpdateRotation(camera))
             {
-                var distance = Vector3.Distance(target, camera.focusPosition);
+                ConstrainAngles(camera);
 
-                var t = 1.0f;
-
-                if(distance > 0.01f && camera.focusCentering > 0)
-                {
-                    t = Math.Pow(1.0f - camera.focusCentering, Time.deltaTime);
-                }
-
-                if(distance > camera.focusRadius)
-                {
-                    t = Math.Min(t, camera.focusRadius / distance);
-                }
-
-                camera.focusPosition = Vector3.Lerp(target, camera.focusPosition, t);
+                rotation = Math.FromEulerAngles(new Vector3(camera.orbitAngles.X, camera.orbitAngles.Y, 0));
+            }
+            else
+            {
+                rotation = transform.LocalRotation;
             }
 
-            var direction = transform.Forward;
+            var direction = Vector3.Transform(new Vector3(0, 0, -1), rotation);
 
-            transform.LocalPosition = camera.focusPosition - direction * camera.distance;
+            var position = camera.focusPosition - direction * camera.distance;
+
+            transform.LocalPosition = position;
+            transform.LocalRotation = rotation;
         });
     }
 
